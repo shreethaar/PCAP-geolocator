b5c2548fada6677f361c40e64e8122c5c0f72353
Shreethaar <shreethaara380@gmail.com>
2024-04-26 22:51:27 +0800

    updates on geolocation function

--- pcapIP_geoLocate.py
+++ pcapIP_geoLocate.py
@@ -6,7 +6,22 @@ def ip_extract(pcap_file):
     packets=rdpcap(pcap_file)
     for p in packets:
         if ip in p:
-            // get source 
-            // get destination
+            ip_address.append(packet[ip].src)
+            ip_address.append(packet[ip].dst)
+        return ip_address
 
+def get_geolocation(ip_addr):
+    reader=Reader('GeoLite2-database')  //need latest version update
+    try: 
+        response=reader.city(ip_addr)
+        country=response.country.name
+        city=response.city.name
+        latitude=response.location.latitude
+        longtitude=response.location.longtitude
+        return country,city,latitude,longitude
+    except:
+        return "INVALID","INVALID",null,null
 
+# next: write ip.addr -> country,city,GPS (could add ASN num) && main function
+# read_pcap
+# extract <- .pcap file
